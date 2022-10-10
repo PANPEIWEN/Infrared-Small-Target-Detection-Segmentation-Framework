85e3216cc74c9ca0ba0ad6e9f049564612658b30
PANPEIWEN <ppw2337354770@gmail.com>
2022-10-10 15:31:34 +0800

    Add data config

--- configs/_base_/datasets/nudt.py
+++ configs/_base_/datasets/nudt.py
@@ -1,13 +1,13 @@
 # dataset settings
 data = dict(
     dataset_type='NUAA',
-    data_root='/data1/ppw/works/All_ISTD/datasets/SIRST_4M',
+    data_root='/data1/ppw/works/All_ISTD/datasets/NUDT',
     base_size=256,
     crop_size=256,
     data_aug=True,
     suffix='png',
     num_workers=8,
-    train_batch=16,
+    train_batch=32,
     test_batch=16,
     train_dir='trainval',
     test_dir='test'
