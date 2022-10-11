a55eb05b2f9a9469449eba677999bc1625834102
PANPEIWEN <ppw2337354770@gmail.com>
2022-10-11 15:36:26 +0800

    Update README.md

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -1,12 +1,16 @@
 # yapf:disable
 log_config = dict(
+    # During the training process, the log is saved every interval epoch.
     interval=10,
+    # Identification role, no practical role
     hooks=[
         dict(type='TextLoggerHook', by_epoch=True),
         dict(type='TensorboardLoggerHook')
     ])
-# yapf:enable
+# Distributed training backend
 dist_params = dict(backend='nccl')
+
+# Identification role, no practical role
 log_level = 'INFO'
 load_from = None
 resume_from = None
